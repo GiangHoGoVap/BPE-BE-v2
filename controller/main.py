e14081be13720bdf94a6080e4d9588956e956c7a
Nguyễn Minh Tâm <minhtam27022001@gmail.com>
2026-03-03 15:28:51 +0700

    Add root route redirect to v1

--- controller/main.py
+++ controller/main.py
@@ -2,6 +2,11 @@ from bpsky import bpsky
 from controller.utils import *
 
 
+@bpsky.route("/", methods=["GET"])
+def root():
+    return redirect("/api/v1/")
+
+
 @bpsky.route("/api/v1/", methods=["GET"])
 def main():
     return "Welcome to BPSky"
